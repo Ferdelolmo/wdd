379de6085e939d478ea15d49b1cc48505763ed15
Ferdelolmo <fernandodelolmovelayos@gmail.com>
2025-09-16 00:02:45 +0200

    fixing italian

--- src/pages/Avila.tsx
+++ src/pages/Avila.tsx
@@ -20,11 +20,11 @@ const Avila = () => {
     },
     it: {
       title: 'Audio Guida',
-      src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2170562355&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+      src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2171146380&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
     },
     scn: {
       title: 'Audio Guida',
-      src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2170562355&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+      src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2171146380&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
     },
     en: {
       title: 'Audio Guide',
